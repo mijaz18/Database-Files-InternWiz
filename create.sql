9fd0259c4582210aac3323c6ec7f2a9c6e912c62
Muhammad Usama Ijaz <mijaz@u.rochester.edu>
2020-03-24 21:34:27 -0400

    added faculty csv file

--- create.sql
+++ create.sql
@@ -65,31 +65,31 @@ CREATE TABLE IF NOT EXISTS 'Referrals' (
 -- Dumping data for table `Students`
 --
 
-INSERT INTO `Students` (`student_id`, `name`, `department`, `email`) VALUES
-(000001, 'David John', 'Fiance', 'djohn@college.edu'),
-(000002, 'Rogers Paul', 'Graphic Design', 'prog10@college.edu'),
-(000003, 'Maria Sanders', 'History', 'msand8@college.edu'),
-(000004, 'Mark Thomas', 'Marketing', 'mthomas15@college.edu'),
-(000005, 'Morris Miller', 'Computer Science', 'mmiller@college.edu'),
-(000006, 'Daniel Michael', 'Marketing', 'dmich@college.edu'),
-(000007, 'Mark Mike', 'Political Science', 'magicmike@college.edu'),
-(000008, 'David John', 'Nursing', 'djohn5@college.edu'),
-(000009, 'Morgan Maria', 'Computer Science', 'mmaria@college.edu'),
-(0000010, 'Paul Sanders', 'History', 'psand@college.edu');
+-- INSERT INTO `Students` (`student_id`, `name`, `department`, `email`) VALUES
+-- (000001, 'David John', 'Fiance', 'djohn@college.edu'),
+-- (000002, 'Rogers Paul', 'Graphic Design', 'prog10@college.edu'),
+-- (000003, 'Maria Sanders', 'History', 'msand8@college.edu'),
+-- (000004, 'Mark Thomas', 'Marketing', 'mthomas15@college.edu'),
+-- (000005, 'Morris Miller', 'Computer Science', 'mmiller@college.edu'),
+-- (000006, 'Daniel Michael', 'Marketing', 'dmich@college.edu'),
+-- (000007, 'Mark Mike', 'Political Science', 'magicmike@college.edu'),
+-- (000008, 'David John', 'Nursing', 'djohn5@college.edu'),
+-- (000009, 'Morgan Maria', 'Computer Science', 'mmaria@college.edu'),
+-- (0000010, 'Paul Sanders', 'History', 'psand@college.edu');
 
 
---
--- Dumping data for table `Students`
---
+-- --
+-- -- Dumping data for table `Students`
+-- --
 
-INSERT INTO `Employers` (`student_id`, `name`, `department`, `email`) VALUES
-(000001, 'David John', 'Fiance', 'djohn@college.edu'),
-(000002, 'Rogers Paul', 'Graphic Design', 'prog10@college.edu'),
-(000003, 'Maria Sanders', 'History', 'msand8@college.edu'),
-(000004, 'Mark Thomas', 'Marketing', 'mthomas15@college.edu'),
-(000005, 'Morris Miller', 'Computer Science', 'mmiller@college.edu'),
-(000006, 'Daniel Michael', 'Marketing', 'dmich@college.edu'),
-(000007, 'Mark Mike', 'Political Science', 'magicmike@college.edu'),
-(000008, 'David John', 'Nursing', 'djohn5@college.edu'),
-(000009, 'Morgan Maria', 'Computer Science', 'mmaria@college.edu'),
-(0000010, 'Paul Sanders', 'History', 'psand@college.edu');
+-- INSERT INTO `Employers` (`student_id`, `name`, `department`, `email`) VALUES
+-- (000001, 'David John', 'Fiance', 'djohn@college.edu'),
+-- (000002, 'Rogers Paul', 'Graphic Design', 'prog10@college.edu'),
+-- (000003, 'Maria Sanders', 'History', 'msand8@college.edu'),
+-- (000004, 'Mark Thomas', 'Marketing', 'mthomas15@college.edu'),
+-- (000005, 'Morris Miller', 'Computer Science', 'mmiller@college.edu'),
+-- (000006, 'Daniel Michael', 'Marketing', 'dmich@college.edu'),
+-- (000007, 'Mark Mike', 'Political Science', 'magicmike@college.edu'),
+-- (000008, 'David John', 'Nursing', 'djohn5@college.edu'),
+-- (000009, 'Morgan Maria', 'Computer Science', 'mmaria@college.edu'),
+-- (0000010, 'Paul Sanders', 'History', 'psand@college.edu');
