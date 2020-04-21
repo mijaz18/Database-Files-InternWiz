e5987873a0a51ef1d9fcc3154b214f2c66b28783
akirarsah <akirarsah@gmail.com>
2020-04-20 22:13:32 -0400

    search_jobs.html added

---
+++ website/search_jobs.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<?php
+require_once('db_setup.php');
+$sql = "USE mijaz_1;";
+if ($conn->query($sql) === TRUE) {
+} else {
+   echo "Error using  database: " . $conn->error;
+}
+
+$employer_name = $_POST['employer_name'];
+$sql = "SELECT FROM (Jobs JOIN Employers) WHERE name = $employer_name;";
+
+$result = $conn->query($sql);
+
+?>
+   <table class="table table-striped">
+      <tr>
+         <th>job_id</th>
+         <th>employer_id</th>
+         <th>description</th>
+         <th>majors</th>
+      </tr>
+<?php
+while($row = $result->fetch_assoc()){
+?>
+      <tr>
+          <td><?php echo $row['job_id']?></td>
+          <td><?php echo $row['employer_id']?></td>
+          <td><?php echo $row['description']?></td>
+          <td><?php echo $row['majors']?></td>
+      </tr>
+
+<?php
+}
+}
+else {
+echo "Nothing to display";
+}
+?>
+
+    </table>
+
+<?php
+$conn->close();
+?>  
+
+</body>
+</html>
