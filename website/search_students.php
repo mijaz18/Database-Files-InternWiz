365fac8647e42ebbd536c1918b21ab52bdbbb701
akirarsah <akirarsah@gmail.com>
2020-04-23 23:31:34 -0400

    search students

---
+++ website/search_students.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<?php
+require_once('db_setup.php');
+$sql = "USE mijaz_1;";
+if ($conn->query($sql) === TRUE) {
+} else {
+   echo "Error using  database: " . $conn->error;
+}
+
+$student_name = $_POST['student_name'];
+$sql = "SELECT * FROM Students WHERE name = '$student_name';";
+
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+
+    ?>
+       <table class="table table-striped">
+          <tr>
+             <th>student_id</th>
+             <th>name</th>
+             <th>department</th>
+             <th>email</th>
+          </tr>
+    <?php
+    while($row = $result->fetch_assoc()){
+    ?>
+          <tr>
+            <td><?php echo $row['student_id']?></td>
+              <td><?php echo $row['name']?></td>
+              <td><?php echo $row['department']?></td>
+              <td><?php echo $row['email']?></td>
+          </tr>
+    
+    <?php
+    }
+    }
+    else {
+    echo "Nothing to display";
+    }
+    ?>
+    
+        </table>
+    
+    <?php
+    $conn->close();
+    ?>  
+    
+    </body>
+    </html>
+    
